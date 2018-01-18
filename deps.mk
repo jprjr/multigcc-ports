ea8de159c1d46c90e30217ac0915d1c6704654fd
John Regan <jregan@mesonet.org>
2018-01-18 09:52:41 -0600

    updates

--- deps.mk
+++ deps.mk
@@ -5,12 +5,14 @@ bash:V: readline ncurses
 binutils:V: zlib
 boost:V: python zlib bzip2 xz
 cmake:V: libarchive python curl
+compiler-rt:V: libcxx
 curl:V: libressl zlib ca-certificates
 dash: autoconf
 gcc:V: binutils libmpc libisl musl zlib
 git:V: zlib curl libexpat pcre2 perl gcc
 libarchive:V: zlib bzip2 xz acl attr libressl
 libbio:V: libutf libfmt
+libcxx:V: libunwind
 libfmt:V: libutf
 libgmp:V: libtool
 libisl:V: libgmp
@@ -20,15 +22,17 @@ libregexp9:V: libutf libfmt
 libressl:V: linux-headers zlib musl
 libtool:V: help2man
 libunwind:V: cmake
-llvm:V: cmake gcc libffi
+llvm:V: cmake libffi compiler-rt libcxx libunwind
 mk:V: libutf libfmt libbio libregexp9
 musl:V: linux-headers
 pm:V: libarchive zlib bzip2 attr acl xz
+perl:V: zlib readline bzip2 libressl xz
 pigz:V: zlib
 python:V: zlib readline bzip2 libressl
 tar:V: pigz bzip2 xz
 x86_64-w64-mingw32-binutils:V: zlib gcc
 x86_64-w64-mingw32-boost:V: x86_64-w64-mingw32-gcc x86_64-w64-mingw32-bzip2 x86_64-w64-mingw32-zlib x86_64-w64-mingw32-xz
+x86_64-w64-mingw32-compiler-rt:V: x86_64-w64-mingw32-cmake
 x86_64-w64-mingw32-mingw:V: x86_64-w64-mingw32-binutils
 x86_64-w64-mingw32-gcc:V: zlib libmpc libisl x86_64-w64-mingw32-binutils x86_64-w64-mingw32-mingw gcc
 x86_64-w64-mingw32-zlib:V: x86_64-w64-mingw32-gcc
