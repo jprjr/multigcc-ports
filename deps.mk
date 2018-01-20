742125cb3e99277b56d2be67ba54a57734705e11
John Regan <jregan@mesonet.org>
2018-01-20 10:30:09 -0600

    some more updates

--- deps.mk
+++ deps.mk
@@ -4,11 +4,11 @@ automake:V: autoconf
 bash:V: readline ncurses
 binutils:V: zlib
 boost:V: python zlib bzip2 xz
-cctools:V: musl-fts xar util-linux libtapi
 cmake:V: libarchive python curl
 compiler-rt:V: libcxx
 curl:V: libressl zlib ca-certificates
-dash: autoconf
+dash:V: autoconf
+execline:V: skalibs
 gcc:V: binutils libmpc libisl musl zlib
 git:V: zlib curl libexpat pcre2 perl gcc
 libarchive:V: zlib bzip2 xz acl attr libressl
@@ -32,9 +32,16 @@ pm:V: libarchive zlib bzip2 attr acl xz
 perl:V: zlib readline bzip2 libressl xz
 pigz:V: zlib
 python:V: zlib readline bzip2 libressl
+s6:V: skalibs execline
+s6-dns:V: skalibs
+s6-networking:V: skalibs execline s6 s6-dns libressl
+s6-overlay: skalibs s6 s6-dns s6-networking s6-portable-utils s6-linux-utils execline
+s6-portable-utils:V: skalibs
+s6-linux-utils:V: skalibs
 tar:V: pigz bzip2 xz
 util-linux:V: linux-headers
 xar:V: libressl libxml2 musl-fts
+x86_64-apple-darwin14-cctools:V: musl-fts xar util-linux libtapi
 x86_64-w64-mingw32-binutils:V: zlib gcc
 x86_64-w64-mingw32-boost:V: x86_64-w64-mingw32-gcc x86_64-w64-mingw32-bzip2 x86_64-w64-mingw32-zlib x86_64-w64-mingw32-xz
 x86_64-w64-mingw32-compiler-rt:V: x86_64-w64-mingw32-cmake
